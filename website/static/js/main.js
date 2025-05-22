454527236a7934ca712e0c9e4d1c78b2753f6d6a
Tiago Martins <tiago.sousams@gmail.com>
2025-05-22 21:16:59 +0100

    Complete start tracking form with date and filter inputs

--- website/static/js/main.js
+++ website/static/js/main.js
@@ -11,10 +11,58 @@ new Awesomplete(input, {
 
 
 // Flatpickr setup
-flatpickr("#minMaxExample", {
+flatpickr("#departureDate", {
+  dateFormat: "d/m/Y",
+  altFormat: "l, d/m/Y",
+  minDate: new Date(), 
+  maxDate: new Date(new Date().setFullYear(new Date().getFullYear() + 1)),
+  locale: "en",
+  autoClose: true,
+  altInput: true
+});
+
+// Flatpickr setup
+flatpickr("#dateRange", {
   mode: "range",
   dateFormat: "d/m/Y",
-  minDate: new Date(new Date().setDate(new Date().getDate() - 8)),
-  maxDate: new Date(new Date().setDate(new Date().getDate() - 1)),
-  locale: "en"
+  altFormat: "l, d/m/Y",
+  minDate: new Date(), 
+  maxDate: new Date(new Date().setFullYear(new Date().getFullYear() + 1)),
+  locale: "en",
+  autoClose: true,
+  altInput: true
 });
+
+
+// Toggle visibility based on checkbox
+/* const checkbox = document.getElementById("isDateRange");
+checkbox.addEventListener("change", function () {
+  const single = document.getElementById("single-date-container");
+  const range = document.getElementById("range-date-container");
+
+  if (checkbox.checked) {
+    single.style.display = "none";
+    range.style.display = "block";
+  } else {
+    single.style.display = "block";
+    range.style.display = "none";
+  }
+}); */
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const checkbox = document.getElementById("isDateRange");
+    const single = document.getElementById("single-date-container");
+    const range = document.getElementById("range-date-container");
+
+    checkbox.addEventListener("change", () => {
+        if (checkbox.checked) {
+            single.classList.add("d-none");
+            range.classList.remove("d-none");
+        } else {
+            single.classList.remove("d-none");
+            range.classList.add("d-none");
+        }
+    });
+});
+
